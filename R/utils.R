8884724ad0e98c4879bf098eee047e4445fef518
Gary Cao <gc3026@gmail.com>
2025-02-20 15:31:58 -0500

    After initial two (three) utils function implementation.

--- R/utils.R
+++ R/utils.R
@@ -23,13 +23,6 @@
 #'
 #' library(flextable)
 #'
-#' ft <- flextable::flextable(head(mtcars[ 1:10]))
-#' tbl_grob <- flextable::gen_grob(
-#'   flextable::flextable(head(mtcars[ 1:10])),
-#'   fit = "auto",
-#'   scaling = "full"
-#' )
-#'
 #'
 #'
 #' @export read_footer
@@ -37,5 +30,78 @@ read_footer <- function(
     filename = filename
     ) {
   if (!file.exists(filename)) stop("Input header_footer file does not exist! Check the filename and/or pathname and try again. \n", filename)
-  readxl::read_xls(filename)
+  tfile <- readxl::read_excel(filename)
+  colnames(tfile) <- toupper(colnames(tfile))
+  required_cols <- c("TYPE", "PGMNAME", "OID", "TTL1", "BYLINE1", "FOOT1")
+  if (!all(required_cols %in% colnames(tfile))) stop("Input file has required column(s) missing.\n")
+  else tfile <- tfile %>% select(TYPE, PGMNAME, OID, starts_with("TTL"), starts_with("BYLINE"), starts_with("FOOT"))
+  #tfile <- tfile[, colSums(is.na(tfile)) != nrow(tfile)]
+
+  return(tfile)
+}
+
+
+#' Select header and footer based on TFL number
+#'
+#' This function select titles and footnotes based on TFL number
+#'
+#'
+#' @details Each TFL (table, listing, or figure) should have a type and TFL number in the header_footer Excel spreadsheet. This information can be in two separate columns (TYPE & TTL1)
+#' or combined in TTL1 column. However, if both type and TFL number are already combined in column TTL1, there should be a space to separate them.
+#' Additional details...
+#'
+#' @export select_with_number
+select_with_number <- function(
+    df = list(),
+    type = NA,
+    tnumber ="" ) {
+  if (is.na(type) & is.na(tnumber)) stop("Selection paramters can not be NULL.\n")
+
+  if (!(toupper(type) %in% c("LISTING", "TABLE", "FIGURE", "GRAPH", '', NA)) ) stop("TLF type is invalid.\n")
+
+  ## if type is not NULL, combine type with tfl number
+  if (!is.na(type)) crit <- paste(type, tnumber) %>% gsub("\\b(\\w+)\\s+\\1\\b", "\\1")
+  else crit <- tnumber
+
+  if (is.na(type))  {
+    footer_list <- df %>% filter(TTL1 == tnumber)
+  } else {
+    footer_list <- df %>% filter(TYPE == type & TTL1 == tnumber)
+  }
+
+  ## make sure only one unique entry is generated
+  if (nrow(footer_list) == 0) stop("No entry is generated. Check the title and footnote file and try again.\n")
+  if (nrow(footer_list) > 1) stop("Non unique entry generated. Check the title and footnote file and try again.\n")
+
+  return(footer_list)
+}
+
+
+#' Select header and footer based on program name and oid
+#'
+#' This function select titles and footnotes based on TFL number
+#'
+#'
+#' @details Each TFL (table, listing, or figure) should have a type and TFL number in the header_footer Excel spreadsheet. This information can be in two separate columns (TYPE & TTL1)
+#' or combined in TTL1 column. However, if both type and TFL number are already combined in column TTL1, there should be a space to separate them.
+#' Additional details...
+#'
+#' @export select_with_number
+select_with_name <- function(
+    df = list(),
+    pname = "",
+    oid ="" ) {
+  if (is.na(pname) | is.na(oid)) stop("Selection paramters are not valid.\n")
+
+  ## if type is not NULL, combine type with tfl number
+  # if (!is.na(type)) crit <- paste(type, tnumber) %>% gsub("\\b(\\w+)\\s+\\1\\b", "\\1")
+  # else crit <- tnumber
+
+  footer_list <- df %>%
+    filter(PGMNAME == pname & OID == oid)
+
+  ## make sure only one unique entry is generated
+  if (nrow(footer_list) != 1) stop("No unique entry generated. Check the title and footnote file and try again.\n")
+
+  return(footer_list)
 }
