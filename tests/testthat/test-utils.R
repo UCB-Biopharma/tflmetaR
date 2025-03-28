f74c1595b8c9c467d8a51b3af1439bb84a63b8e2
E643795 <e643795@GDCRVALAP002.dir.ucb-group.com>
2025-03-28 17:42:36 +0100

    Use a relative path to access the sample Excel file

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -10,5 +10,7 @@ test_that("readxl throws an error if the file does not exist", {
 
 #test incorrect sheetname
 test_that("read_footer throws an error if incorrect sheetname given", {
-  expect_error(TFootr::read_footer("~/TFootr/inst/extdata/sample_titles.xls", sheetname = "foot"), "Failed to read the sheet. Please check the file and sheet name.")
+  file_name <- file.path("../../inst/extdata", "sample_titles.xls")
+  expect_error(TFootr::read_footer(file_name, sheetname = "foot"), "Failed to read the sheet. Please check the file and sheet name.")
 })
+
